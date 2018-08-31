ec2e7bd590736fe3c6efa1f61587af67296e7931
Wellington Costa <wcosta.ale@gmail.com>
2018-08-31 16:47:15 -0300

    Projeto base

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBoxMenssagen = new System.Windows.Forms.RichTextBox();
+            this.textBoxChave = new System.Windows.Forms.TextBox();
+            this.richTextBoxSaida = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.error)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxMenssagen
+            // 
+            this.richTextBoxMenssagen.Location = new System.Drawing.Point(14, 77);
+            this.richTextBoxMenssagen.Name = "richTextBoxMenssagen";
+            this.richTextBoxMenssagen.Size = new System.Drawing.Size(391, 110);
+            this.richTextBoxMenssagen.TabIndex = 1;
+            this.richTextBoxMenssagen.Text = "";
+            // 
+            // textBoxChave
+            // 
+            this.textBoxChave.Location = new System.Drawing.Point(13, 29);
+            this.textBoxChave.Name = "textBoxChave";
+            this.textBoxChave.Size = new System.Drawing.Size(391, 23);
+            this.textBoxChave.TabIndex = 0;
+            // 
+            // richTextBoxSaida
+            // 
+            this.richTextBoxSaida.Location = new System.Drawing.Point(14, 262);
+            this.richTextBoxSaida.Name = "richTextBoxSaida";
+            this.richTextBoxSaida.ReadOnly = true;
+            this.richTextBoxSaida.Size = new System.Drawing.Size(391, 110);
+            this.richTextBoxSaida.TabIndex = 4;
+            this.richTextBoxSaida.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cripto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Cripto);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Chave";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(317, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mensagem";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado";
+            // 
+            // error
+            // 
+            this.error.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.error.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 393);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBoxSaida);
+            this.Controls.Add(this.textBoxChave);
+            this.Controls.Add(this.richTextBoxMenssagen);
+            this.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Criptografia de Vegenère";
+            ((System.ComponentModel.ISupportInitialize)(this.error)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxMenssagen;
+        private System.Windows.Forms.TextBox textBoxChave;
+        private System.Windows.Forms.RichTextBox richTextBoxSaida;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider error;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;                               /***********************************/
+using System.Collections.Generic;           /** Autor: Wellington             **/
+using System.ComponentModel;                /** Disciplina: GSD, Prof. Joao   **/
+using System.Data;                          /** Criptografia de Vigenere      **/
+using System.Drawing;                       /***********************************/
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Cripto(object sender, EventArgs e)
+        {
+            //virificação de campos vazios
+            error.Clear();
+            bool erro = false;
+           
+            if (textBoxChave.Text == "")
+            {
+                error.SetError(textBoxChave, "Não esta preenchido!");
+                erro = true;
+            }        
+            if (richTextBoxMenssagen.Text == "")
+            {
+                error.SetError(richTextBoxMenssagen, "Não esta preenchido!");
+                erro = true;
+            }
+            
+            if(textBoxChave.Text != "" && richTextBoxMenssagen.Text !="")
+                erro = false;
+
+            //se os campos forem preenchidos executa a criptografia
+            if (erro == false)
+            {
+
+                char[] alfabeto = new char[26] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+                List<int> ListaChave = new List<int>();
+                List<int> ListaMensagem = new List<int>();
+
+                string chave = textBoxChave.Text.ToLower().Trim();
+                chave = chave.Replace(" ", "");
+                string mensagem = richTextBoxMenssagen.Text.ToLower().Trim();
+
+                string cripto = " ";
+
+                for (int i = 0; i < mensagem.Length; i++)
+                {
+                    //verifica se na mensagem tem um espaço em branco
+                    if (mensagem[i] == ' ')
+                        ListaMensagem.Add(32);
+
+                    //percorre o alfabeto
+                    for (int j = 0; j < alfabeto.Length; j++)
+                    {
+                        //verifica se a letra da mensagem é igual a do alfabeto
+                        //caso seja quarda na ListMensagem a posicao da letra situada no alfabeto
+                        if (mensagem[i] == alfabeto[j])
+                            ListaMensagem.Add(j);
+
+                    }
+                }
+
+                int aux = 0;
+                
+                for (int i = 0; i < ListaMensagem.Count; i++)
+                {
+                    //verifica se na mensagem tem um espaço em branco
+                    //se tiver adiciona também na mesma posição na listaChave
+                    if (ListaMensagem[i] == 32)
+                        ListaChave.Add(32); 
+
+                    for (int j = 0; j < alfabeto.Length; j++)
+                    {
+                        //verifica se o tamnho da mensagem é menor que o tamanho da chave
+                        //ou se estorou o indice da chave[i]
+                        if (i < chave.Length)
+                        {
+                            //verifica se a letra da chave é igual a do alfabeto
+                            //caso seja quarda no ListChave a posicao da letra situada no alfabeto
+                            if (chave[i] == alfabeto[j])
+                                ListaChave.Add(j);
+                        }
+                        else
+                        {                            
+                            //caso tenha estorado o ince da chave[i], a variavel "aux" é quem controla a lista
+                            //verifica se o aux é menor que a mensagem
+                            if (aux < ListaMensagem.Count)
+                            {
+
+                                //verifica se na mensagem tem um espaço em branco
+                                //se tiver adiciona também na mesma posição na listaChave
+                                if (ListaMensagem[aux] == 32)
+                                    ListaChave.Add(32);
+                            
+                                //verifica se as listas são do mesmo tamanho
+                                if (ListaChave.Count != ListaMensagem.Count)
+                                {
+                                    //verifica se o aux esta dentro dos limites da chave
+                                    if (aux < chave.Length)
+                                    {
+                                        //como a mensagem é maior que a chave adiciona repetidamente até que os dois tenha o mesmo tamanho
+                                        //verifica se a letra da chave é igual a do alfabeto caso seja quarda no ListChave a posicao da letra 
+                                        //situada no alfabeto
+                                        if (chave[aux] == alfabeto[j])
+                                        {
+                                            ListaChave.Add(j);
+                                            aux++;
+                                        }
+                                    }
+                                    else
+                                        aux = 0;
+                                }
+
+                            }
+
+                        }//12 20 8 19 14
+                    }
+                }
+
+                for (int i = 0; i < ListaMensagem.Count; i++)
+                {
+                    //verifica se na ListaMensagem tem um "espaço" = 32 no ASCII
+                    //caso tenha adiciona no resultado da cripto
+                    if (ListaMensagem[i] == 32 && ListaChave[i] == 32)
+                        cripto += " ";
+                    else
+                    {
+                        //aplica a formula da criptografia 
+                        int posicao = (ListaMensagem[i] + ListaChave[i]) % 26;
+                        cripto += alfabeto[posicao];
+                    }
+                }
+                //mostra o resultado da criptografia
+                richTextBoxSaida.Text = cripto;
+            }
+        }
+        //Limpar os campos
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxChave.Text = "";
+            richTextBoxMenssagen.Text = "";
+            richTextBoxSaida.Text = "";
+            error.Clear();
+        }
+        
+    }
+}
